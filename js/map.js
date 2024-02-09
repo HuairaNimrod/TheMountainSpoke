e065dad67963899025641fa319d289f18574061f
Nimrod Huaira <nimax29@gmail.com>
2024-02-09 03:49:31 -0700

    done

---
+++ js/map.js
@@ -0,0 +1,12 @@
+function initMap() {
+        var uluru = {lat: 45.672483, lng: -111.090718};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
